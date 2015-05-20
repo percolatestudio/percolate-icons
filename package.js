be3d4cbd7057bc25587c8bb3b731d5454736ffbb
Tim Hingston <tim.hingston@gmail.com>
2015-05-20 14:50:14 +1000

    updated to satisfy linting rules

--- package.js
+++ package.js
@@ -1,17 +1,18 @@
+
 var Version = "v1.9";
 
 Package.describe({
   summary: "Icons for Percolate, version" + Version
 });
 
-Package.on_use(function (api) {
-  var path = Npm.require('path');
-  var asset_path = path.join(Version);
+Package.on_use(function(api) {
+  var path = Npm.require("path");
+  var assetPath = path.join(Version);
 
-  api.add_files(path.join(asset_path, 'fonts', 'percolate.eot'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'percolate.svg'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'percolate.ttf'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'percolate.woff'), 'client');
+  api.add_files(path.join(assetPath, "fonts", "percolate.eot"), "client");
+  api.add_files(path.join(assetPath, "fonts", "percolate.svg"), "client");
+  api.add_files(path.join(assetPath, "fonts", "percolate.ttf"), "client");
+  api.add_files(path.join(assetPath, "fonts", "percolate.woff"), "client");
 
-  api.add_files(path.join('edited-style.css'), 'client');
+  api.add_files(path.join("edited-style.css"), "client");
 });
