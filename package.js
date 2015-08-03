b51d471c3de2d8d79d6385e8129d978584acbf1f
Tom Coleman <tom@thesnail.org>
2015-08-03 11:41:43 +1000

    Update to new syntax

--- package.js
+++ package.js
@@ -8,10 +8,10 @@ Package.on_use(function (api) {
   var path = Npm.require('path');
   var asset_path = path.join(Version);
 
-  api.add_files(path.join(asset_path, 'fonts', 'percolate.eot'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'percolate.svg'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'percolate.ttf'), 'client');
-  api.add_files(path.join(asset_path, 'fonts', 'percolate.woff'), 'client');
+  api.add_files(path.join(asset_path, 'fonts', 'percolate.eot'), 'client', {isAsset: true});
+  api.add_files(path.join(asset_path, 'fonts', 'percolate.svg'), 'client', {isAsset: true});
+  api.add_files(path.join(asset_path, 'fonts', 'percolate.ttf'), 'client', {isAsset: true});
+  api.add_files(path.join(asset_path, 'fonts', 'percolate.woff'), 'client', {isAsset: true});
 
   api.add_files(path.join('edited-style.css'), 'client');
 });
